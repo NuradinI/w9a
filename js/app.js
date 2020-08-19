b24f438e3d15fe0b5e2dd447bb0e3430b317494d
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-08-18 20:33:11 -0600

    created project skeleton

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var userAge = 18;
+var is_subscribed = true;
+
+if(userAge >=18) {
+   console.log("user is old enough to view content!");
+}else{
+    console.log("user is not old enough for this grow up lol");
+}
+
+if(is_subscribed) {
+    console.log("user is subed");
+}else{
+    console.log("user is not subed")
+}
